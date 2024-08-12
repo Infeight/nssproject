a184ba85e91e390c2e4aee48e9bc8c2f4d50f4e5
Infeight <167733649+Infeight@users.noreply.github.com>
2024-08-13 00:09:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
-app.options('*', (req, res) => {
+app.options('https://nssiiitdmkurnool.netlify.app', (req, res) => {
   res.sendStatus(200);
 });
 app.use(express.json())
